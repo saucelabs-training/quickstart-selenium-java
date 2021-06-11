d87d42253ef558925ddacc6dbb6fbbd9f4ff60d3
Lindsay Walker <lindsay.waklker@saucelabs.com>
2021-06-11 10:29:50 -0500

    created an example with browser options:

--- src/test/java/com/saucelabs/quickstart/SauceTest.java
+++ src/test/java/com/saucelabs/quickstart/SauceTest.java
@@ -1,5 +1,6 @@
 package com.saucelabs.quickstart;
 
+import com.saucelabs.saucebindings.options.SauceOptions;
 import com.saucelabs.saucebindings.junit4.SauceBaseTest;
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,4 +12,16 @@ public class SauceTest extends SauceBaseTest {
         driver.navigate().to("https://www.saucedemo.com");
         Assert.assertEquals(driver.getTitle(), "Swag Labs");
     }
+    public SauceOptions createSauceOptions() {
+        return SauceOptions.firefox()
+                .setExtendedDebugging(true)
+                .build();
+    }
+
+    public SauceOptions createSauceOptions2() {
+        return SauceOptions.ie()
+                .setPrerun()
+                .build();
+    }
+
 }
